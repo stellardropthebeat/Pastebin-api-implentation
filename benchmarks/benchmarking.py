4a5a23fed4b0090f24aaf735f743dbaccdb02db2
DROPTHEBEAT44 <nutchapol1111@gmail.com>
2022-05-26 11:33:09 +0700

    Finish benchmarking all /api reach 500reqs/sec

--- benchmarks/benchmarking.py
+++ benchmarks/benchmarking.py
@@ -6,24 +6,23 @@ import random
 
 class benchmarking(FastHttpUser):
 
-    # @task
-    # def test_home(self):
-    #    self.client.get('')
+    @task
+    def test_home(self):
+        self.client.get('')
 
-    # @task
-    # def test_api_paste(self):
-    #    payload = {
-    #        "title": "My New paste",
-    #        "content": "GGWP"
-    #    }
-    #    headers = {'content-type': 'application/json'}
-    #
-    #    self.client.post("/api/paste", data=json.dumps(payload), headers=headers)
+    @task
+    def test_api_paste(self):
+        payload = {
+            "title": "My New paste",
+            "content": "GGWP"
+        }
+        headers = {'content-type': 'application/json'}
+        #
+        self.client.post("/api/paste", data=json.dumps(payload), headers=headers)
 
-    #@task
-    #def test_id(self):
-    #    self.client.get('/api/1')
-
+    @task
+    def test_id(self):
+        self.client.get('/api/1')
 
     @task
     def test_api_recents(self):
